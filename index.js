fabbdaf81b505a6a6afd358b2056cddfeb2772af
Wambui <nderituwambui93@gmail.com>
2024-04-16 11:25:58 +0300

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(array) {
+    const win = array.find(game => {
+      console.log(`Checking year ${game.year}...`);
+      return game.result === "W";
+    });
+    console.log("Win:", win);
+    return win ? win.year : undefined;
+  }
+  
